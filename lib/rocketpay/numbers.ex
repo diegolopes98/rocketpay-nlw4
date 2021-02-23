3007648ffe16e5270dbfb9bc8af96cf22c927e4c
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-23 11:47:43 -0300

    refactor: use Strem to lazy evaluate map in Numbers usecase

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -9,7 +9,7 @@ defmodule Rocketpay.Numbers do
     result =
       result
       |> String.split(",")
-      |> Enum.map(&(String.to_integer(&1)))
+      |> Stream.map(&(String.to_integer(&1)))
       |> Enum.sum()
 
     {:ok, %{result: result}}
