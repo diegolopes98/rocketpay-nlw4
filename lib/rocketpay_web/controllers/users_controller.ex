542300d26c4b78c2c77514a7d4b19f844e5b161c
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-24 15:20:52 -0300

    feat: add fallback controller

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -3,6 +3,8 @@ defmodule RocketpayWeb.UsersController do
 
   alias Rocketpay.User
 
+  action_fallback RocketpayWeb.FallbackController
+
   def create conn, params do
     params
     |> Rocketpay.create_user()
@@ -16,10 +18,5 @@ defmodule RocketpayWeb.UsersController do
     |> render("create.json", user: user)
   end
 
-  defp handle_response({:error, error}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(RocketpayWeb.ErrorView)
-    |> render("400.json", result: error)
-  end
+  defp handle_response({:error, _result} = error, _conn), do: error
 end
