0ac1d7d19c9f0da92cb27abde892456e655f179e
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-25 12:49:49 -0300

    feat: add Transaction response struct

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,6 +1,7 @@
 defmodule RocketpayWeb.AccountsView do
 
   alias Rocketpay.{Account}
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   def render("update.json", %{
     account: %Account{id: id, balance: balance}
@@ -15,7 +16,7 @@ defmodule RocketpayWeb.AccountsView do
   end
 
   def render("transaction.json", %{
-    transaction: %{to_account: to_account, from_account: from_account}
+    transaction: %TransactionResponse{to_account: to_account, from_account: from_account}
   }) do
     %{
       message: "Transaction done successfully",
