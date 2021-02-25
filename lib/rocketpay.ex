3849c1aa88e4c082c9f8b78a1ce50e90dc260349
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-25 11:46:32 -0300

    feat: add withdraw

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,8 +1,10 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: CreateUser
   alias Rocketpay.Accounts.Deposit
+  alias Rocketpay.Accounts.Withdraw
 
   defdelegate create_user(params), to: CreateUser, as: :call
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 
 end
