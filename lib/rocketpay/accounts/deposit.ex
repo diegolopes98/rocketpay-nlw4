63124d55516fca210c847949ea6d57e763611b97
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-02-25 11:58:39 -0300

    feat: add operation to handle duplicated code

--- lib/rocketpay/accounts/deposit.ex
+++ lib/rocketpay/accounts/deposit.ex
@@ -1,47 +1,11 @@
 defmodule Rocketpay.Accounts.Deposit do
-  alias Ecto.Multi
-  alias Rocketpay.{Account, Repo}
+  alias Rocketpay.Accounts.Operation
+  alias Rocketpay.Repo
 
-  def call  %{"id" => id, "value" => value} do
-    Multi.new()
-    |> Multi.run(:account, fn repo, _ ->
-      get_account(repo, id)
-    end)
-    |> Multi.run(:update_balance, fn repo, %{account: account} ->
-      update_balance(repo, account, value)
-    end)
-    |> run_transaction()
-  end
-
-
-  defp get_account repo, id do
-    case repo.get(Account, id) do
-      nil -> {:error, "Account not found!"}
-      account -> {:ok, account}
-    end
-  end
-
-  defp update_balance repo, account, value do
-    account
-    |> sum_values(value)
-    |> update_account(repo, account)
-  end
-
-  defp sum_values %Account{balance: balance}, value do
-    value
-    |> Decimal.cast()
-    |> handle_cast(balance)
-  end
-
-  defp handle_cast({:ok, value}, balance), do: Decimal.add(value, balance)
-  defp handle_cast(:error, _balance), do: {:error, "Invalid deposit value"}
-
-  defp update_account({:error, _} = error, _repo, _account), do: error
-  defp update_account(value, repo, account) do
-    params = %{balance: value}
+  def call params do
     params
-    |> Account.changeset(account)
-    |> repo.update()
+    |> Operation.call(:deposit)
+    |> run_transaction()
   end
 
   defp run_transaction multi do
